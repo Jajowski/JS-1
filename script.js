d468589903f00fce6fd6a7ac864ed7ead5d47ef9
Jajowski <tamojajanidze26@gmail.com>
2023-04-28 22:58:51 +0400

    First commi

---
+++ script.js
@@ -0,0 +1,25 @@
+let number = 10;
+if (number <50){
+    console.log("<50")
+}
+else if(number>20){
+    console.log(">20")
+}
+else{
+    console.log("shecdoma")
+}
+
+        let sakheli ='mariam';
+        if (sakheli=='mariam'){
+            console.log(true)
+        }
+        else{
+            console.log(false)
+        }
+
+        let saxeli='mariam'
+        switch(saxeli){
+            case"mariam":console.log('true'); 
+            break;
+            default:console.log('false')
+        }
